3156b9647af074c5b0d6f7f55f306649b2e897d5
harry79ph <73724613+harry79ph@users.noreply.github.com>
2021-03-25 22:13:12 +0000

    Update App.js
    Added "Please increase the PG ratio." warning

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Details from './Details';
 
 class App extends React.Component {
+  
   state = { amount: 10, pg: 30, vg: 70, flav: 0, nicBase: 72, nic: 0, };
   warning = '';
-
+  
   get ing() {
     let nic_ml, flav_ml, pg_ml, vg_ml;
     if (this.state.amount > 0 && this.state.pg >= 0 && this.state.vg >= 0 && this.state.flav >= 0 && this.state.nicBase >= 0 && this.state.nic >= 0) {
@@ -15,20 +16,20 @@ class App extends React.Component {
       }
       flav_ml = (this.state.flav / 100) * this.state.amount;
       if (parseInt(this.state.pg) + parseInt(this.state.vg) !== 100) {
-        pg_ml = 0;
-        vg_ml = 0;
+        [pg_ml, vg_ml] = [0, 0];
         this.warning = 'Please make sure that the total ratio of VG and PG is 100%.';
       } else {
         pg_ml = ((this.state.pg - this.state.flav) / 100) * this.state.amount - nic_ml;
         vg_ml = (this.state.vg / 100) * this.state.amount;
-        this.warning = '';
+        if (pg_ml < 0) {
+          this.warning = 'Please increase the PG ratio.';
+        } else {
+          this.warning = '';
+        }
       }
       return { pg_ml, vg_ml, flav_ml, nic_ml };
     } else {
-      nic_ml = 0;
-      flav_ml = 0;
-      pg_ml = 0;
-      vg_ml = 0;
+      [nic_ml, flav_ml, pg_ml, vg_ml] = [0, 0, 0, 0];
       return { pg_ml, vg_ml, flav_ml, nic_ml };
     }
   }
