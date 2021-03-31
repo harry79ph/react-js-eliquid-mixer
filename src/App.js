12c9c33fba0083351dd88a383492c30703b04afe
harry79ph <73724613+harry79ph@users.noreply.github.com>
2021-03-31 01:27:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,57 +3,46 @@ import Details from './Details';
 
 class App extends React.Component {
   
-  state = { amount: 10, pg: 30, vg: 70, flav: 0, nicBase: 72, nic: 0, };
-  warning = '';
+  state = { amount: 10, pg: 30, vg: 70, flav: 0, nicBase: 72, nic: 0 };
   
   get ing() {
-    let nic_ml, flav_ml, pg_ml, vg_ml;
+    let nic_ml, flav_ml, pg_ml, vg_ml, warning = '';
     if (this.state.amount > 0 && this.state.pg >= 0 && this.state.vg >= 0 && this.state.flav >= 0 && this.state.nicBase >= 0 && this.state.nic >= 0) {
-      if (this.state.nicBase > 0) {
-        nic_ml = (this.state.nic * this.state.amount) / this.state.nicBase;
-      } else {
-        nic_ml = 0;
-      }
+      if (this.state.nicBase > 0) nic_ml = (this.state.nic * this.state.amount) / this.state.nicBase;
+      else nic_ml = 0;
       flav_ml = (this.state.flav / 100) * this.state.amount;
       if (parseInt(this.state.pg) + parseInt(this.state.vg) !== 100) {
         [pg_ml, vg_ml] = [0, 0];
-        this.warning = 'Please make sure that the total ratio of VG and PG is 100%.';
+        warning = 'Please make sure that the total ratio of VG and PG is 100%.';
       } else {
         pg_ml = ((this.state.pg - this.state.flav) / 100) * this.state.amount - nic_ml;
         vg_ml = (this.state.vg / 100) * this.state.amount;
-        if (pg_ml < 0) {
-          this.warning = 'Please increase the PG ratio.';
-        } else {
-          this.warning = '';
-        }
+        if (pg_ml < 0) warning = 'Please increase PG ratio.';
+        else warning = '';
       }
-      return { pg_ml, vg_ml, flav_ml, nic_ml };
-    } else {
-      [nic_ml, flav_ml, pg_ml, vg_ml] = [0, 0, 0, 0];
-      return { pg_ml, vg_ml, flav_ml, nic_ml };
-    }
+    } else [nic_ml, flav_ml, pg_ml, vg_ml] = [0, 0, 0, 0];
+    return { pg_ml, vg_ml, flav_ml, nic_ml, warning };
   }
   
   render() {
     const items = [
-      { desc: 'Total Amount (ml):', id: 'des-amount', value: this.state.amount,
-      inputHandler: e => this.setState({ amount: e.target.value }), key: 1 },
-      { desc: 'PG Ratio (%):', id: 'des-pg', value: this.state.pg,
-      inputHandler: e => this.setState({ pg: e.target.value }), key: 2 },
-      { desc: 'VG Ratio (%):', id: 'des-vg', value: this.state.vg,
-      inputHandler: e => this.setState({ vg: e.target.value }), key: 3 },
-      { desc: 'Desired Flavour (%):', id: 'des-flav', value: this.state.flav,
-      inputHandler: e => this.setState({ flav: e.target.value }), key: 4 },
-      { desc: 'Nicotine Strength (mg):', id: 'nic-base', value: this.state.nicBase,
-      inputHandler: e => this.setState({ nicBase: e.target.value }), key: 5 },
-      { desc: 'Desired Strength (mg):', id: 'des-nic', value: this.state.nic,
-      inputHandler: e => this.setState({ nic: e.target.value }), key: 6 }
+      { desc: 'Total Amount (ml):', name: 'amount', value: this.state.amount, key: 1 },
+      { desc: 'PG Ratio (%):', name: 'pg', value: this.state.pg, key: 2 },
+      { desc: 'VG Ratio (%):', name: 'vg', value: this.state.vg, key: 3 },
+      { desc: 'Desired Flavour (%):', name: 'flav', value: this.state.flav, key: 4 },
+      { desc: 'Nicotine Strength (mg):', name: 'nicBase', value: this.state.nicBase, key: 5 },
+      { desc: 'Desired Strength (mg):', name: 'nic', value: this.state.nic, key: 6 }
     ];
+    const handleChange = (e) =>  {
+      let name = e.target.name;
+      let value =  e.target.value;
+      this.setState({[name]: value});
+    }
     const list = items.map((item, key) => {
       return (
         <div className="ing" key={key}>
-          <label htmlFor={item.id}>{item.desc}</label>
-          <input id={item.id} type="text" value={item.value} onChange={item.inputHandler} />
+          <label htmlFor={item.name}>{item.desc}</label>
+          <input name={item.name} type="text" value={item.value} onChange={handleChange} />
         </div>
       );
     });
@@ -96,7 +85,7 @@ class App extends React.Component {
             </tbody>
           </table>
         </div>
-        <p style={{ fontWeight: "bold" }}>{this.warning}</p>
+        <p style={{ fontWeight: "bold" }}>{this.ing.warning}</p>
         <p>Note: Apart from VG dilutant, all the other ingredients should be PG based.</p>
       </div>
     );
