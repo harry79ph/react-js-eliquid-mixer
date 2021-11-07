1335900825a99f6df4d19be455fdc473c0885a64
harry79ph <serdare_79@hotmail.com>
2021-11-07 23:24:35 +0000

    minor changes

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 class Details extends React.Component{
-    
+    constructor(props) {
+        super(props);
+        console.log(this.props);//it will call the log before the component is mounted
+    }
     render(){
+        
         return (
             <tr>
                 <td>{this.props.ingredient}</td>
